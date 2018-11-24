999a8caf11ae2059b849d3fc51f65d95ac79a9b4
Jeromy Cannon <jeromylewiscannon@gmail.com>
2018-11-23 23:35:18 -0600

    got rings working!

--- addons/rings/rings.lua
+++ addons/rings/rings.lua
@@ -29,11 +29,7 @@ _addon.command = "rings"
 _addon.commands = {"rings"}
 _addon.version = "1.0"
 
-res = require 'resources'
-
-function match_item(target_item,m)
-    return type(m) == 'table' and m.id and res.items[m.id] and (res.items[m.id].en:lower() == target_item:lower() or res.items[m.id].enl:lower() == target_item:lower())
-end
+res = require "resources"
 
 function print_obj(obj, hierarchy_level)
     if (hierarchy_level == nil) then
@@ -46,42 +42,103 @@ function print_obj(obj, hierarchy_level)
     for i = 0, hierarchy_level, 1 do
         whitespace = whitespace .. "-"
     end
-    windower.add_to_chat(208, whitespace)
+    --my_print(whitespace)
 
-    --windower.add_to_chat(208, obj)
+    --my_print(obj)
     if (type(obj) == "table") then
         for k, v in pairs(obj) do
-            windower.add_to_chat(208, whitespace .. "-")
+            --my_print(whitespace .. "-")
             if (type(v) == "table") then
                 print_obj(v, hierarchy_level + 1)
             else
-                windower.add_to_chat(208, k .. ":" .. v)
+                my_print(whitespace .. "-" .. tostring(k) .. ":" .. tostring(v))
             end
         end
     else
-        --windower.add_to_chat(208, obj)
+        --my_print(obj)
     end
+end
+
+function my_print(...)
+    print_result = ""
+    for i, v in ipairs(arg) do
+        if v then
+            print_result = print_result .. tostring(v) .. ">"
+        else
+            print_result = print_result .. tostring("nil") .. ">"
+        end
+    end
+    windower.add_to_chat(208, print_result)
+end
+
+function my_debug(...)
+    local myLoc = debug.getinfo(2, "S")
+    local myLoc2 = debug.getinfo(2, "n")
+    if not myLoc2.name then
+        myLoc2.name = "undefined"
+    end
+    print_result = ""
+    for i, v in ipairs(arg) do
+        if v then
+            print_result = print_result .. tostring(v) .. ">"
+        else
+            print_result = print_result .. tostring("nil") .. ">"
+        end
+    end
+    my_print(
+        string.format("%.2f:", os.clock()) ..
+            myLoc.short_src .. ":" .. myLoc.linedefined .. ":" .. myLoc2.name .. ":" .. print_result
+    )
+end
+
+function match_item(target_item, m)
+    return type(m) == "table" and m.id and res.items[m.id] and
+        (res.items[m.id].en:lower() == target_item:lower() or res.items[m.id].enl:lower() == target_item:lower())
 end
 
 function ring_lookup(ring)
     wardrobe = windower.ffxi.get_items(8)
     for slot, item in pairs(wardrobe) do
-        windower.add_to_chat(208, slot..':'..item)
+        if item then
+            if type(item) == "table" and res.items[item.id] then
+                item_desc = res.items[item.id].en:lower()
+                --my_print("slot", slot, "item.id", item.id, "name", item_desc)
+                if item_desc == ring:lower() then
+                    --my_print("found it!!!")
+                    ring_location = {
+                        bag_loc = slot,
+                        id = item.id,
+                        description = item_desc,
+                        cast_delay = res.items[item.id].cast_delay
+                    }
+                    return ring_location
+                end
+            else
+                -- my_print("slot", slot, "items...>")
+                -- print_obj(items)
+            end
+        end
     end
-    print_obj(wardrobe)
+    --print_obj(wardrobe)
 end
 
 function main(ring, ...)
     the_ring = ring_lookup(ring)
     if the_ring == nil then
-        windower.add_to_chat(208, ring .. " not found")
+        my_print(ring .. " not found")
         return
     end
-    windower.add_to_chat(208, ring .. ":" .. rings[ring])
-    equip = windower.ffxi.get_items().equipment
-    windower.add_to_chat(208, tostring(equip.right_ring))
+    my_print(the_ring.bag_loc, the_ring.id, the_ring.description, the_ring.cast_delay)
+    windower.ffxi.set_equip(the_ring.bag_loc, 14, 8)
+    for i = the_ring.cast_delay + 5, 1, -1 do
+        my_print(ring .. " countdown " .. i)
+        coroutine.sleep(1)
+    end
+    windower.send_command('input /item "' .. ring .. '" <me>')
+    --equip = windower.ffxi.get_items().equipment
+    --my_print(tostring(equip.right_ring))
 
-    --windower.add_to_chat(208, print_obj(equip))
+    --my_print(print_obj(equip))
 end
 
 windower.register_event("addon command", main)
